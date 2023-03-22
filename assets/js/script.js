86e7869f35bc070cf3a3c190838fa0428d5e7c6c
Seer <kmkevins97@gmail.com>
2023-03-21 21:31:06 -0700

    Added search functionality to map

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,6 +8,36 @@ function initMap() {
     zoom: 5,
     center: california,
     });
+
+    var searchBox = new google.maps.places.SearchBox(
+        document.getElementById("locationSearch")
+      );
+      google.maps.event.addListener(searchBox, "places_changed", function () {
+        searchBox.set("map", null);
+    
+        var places = searchBox.getPlaces();
+    
+        var bounds = new google.maps.LatLngBounds();
+        var i, place;
+        for (i = 0; (place = places[i]); i++) {
+          (function (place) {
+            var marker = new google.maps.Marker({
+              position: place.geometry.location,
+            });
+            marker.bindTo("map", searchBox, "map");
+            google.maps.event.addListener(marker, "map_changed", function () {
+              if (!this.getMap()) {
+                this.unbindAll();
+              }
+            });
+            bounds.extend(place.geometry.location);
+          })(place);
+        }
+        map.fitBounds(bounds);
+        searchBox.set("map", map);
+        map.setZoom(Math.min(map.getZoom(), 12));
+      });
+ 
 }
 
 
